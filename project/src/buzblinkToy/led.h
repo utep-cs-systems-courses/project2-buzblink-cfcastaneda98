9d89a4dc15fdca44cb04dc861da9cbcf4618036c
cfcastaneda98 <cfcastaneda@miners.utep.edu>
2020-10-31 20:37:22 -0600

    Have started to label the different states of the red and green LEDs to be used in the toy.

--- project/src/buzblinkToy/led.h
+++ project/src/buzblinkToy/led.h
@@ -5,12 +5,28 @@
 #define LED_GREEN BIT6             // P1.6
 #define LEDS (BIT0 | BIT6)
 
-
+//This act as the boolean flags of the program
 extern unsigned char red_on, green_on;
 extern unsigned char led_changed;
+
+char redOn;
+char greenOn;
 
 void led_init();
 void led_update();
 
+//Different modes used by the red LED
+void activate_red();
+void deactivate_red();
+void dim_red();
+void normal_red();
+void bright red();
+
+//Different modes used by the green LED
+void activate_green();
+void deactivate_green();
+void dim_green();
+void normal_green();
+void bright_green();
 
 #endif // included
