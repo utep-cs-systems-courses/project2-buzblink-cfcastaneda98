5e67cc86cfc86ace4028c8f0e9e0ad0ba36c0092
cfcastaneda98 <cfcastaneda@miners.utep.edu>
2020-11-01 12:55:40 -0700

    Defined more characteristics to introduce the buttons seen on the green board so I may used them soon.

--- project/src/buzblinkToy/switches.c
+++ project/src/buzblinkToy/switches.c
@@ -8,9 +8,12 @@ static char
 switch_update_interrupt_sense()
 {
   char p1val = P1IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
   P1IES |= (p1val & SWITCHES);/* if switch up, sense down */
   P1IES &= (p1val | ~SWITCHES);/* if switch down, sense up */
+  P2IES |= (p2val & SWITCHES2);
+  P2IES &= (p2val | ~SWITCHES2);
   return p1val;
 }
 
@@ -19,9 +22,13 @@ void
 switch_init()/* setup switch */
 {
   P1REN |= SWITCHES;/* enables resistors for switches */
+  P2REN |= SWITCHES2;
   P1IE |= SWITCHES;/* enable interrupts from switches */
+  P2IE |= SWITCHES2;
   P1OUT |= SWITCHES;/* pull-ups for switches */
+  P2OUT |= SWITCHES2;
   P1DIR &= ~SWITCHES;/* set switches' bits for input */
+  P2DIR &= ~SWITCHES2;
   switch_update_interrupt_sense();
   led_update();
 }
--- project/src/buzblinkToy/switches.h
+++ project/src/buzblinkToy/switches.h
@@ -5,6 +5,12 @@
 #define SW1 BIT3/* switch1 is p1.3 */
 #define SWITCHES SW1/* only 1 switch on this board */
 
+//Switches from green board
+#define S1 BIT0
+#define S2 BIT1
+#define S3 BIT2
+#define S4 BIT3
+#define SWITCHES2 (S1 | S2 | S3 | S4)
 
 void switch_init();
 void switch_interrupt_handler();
