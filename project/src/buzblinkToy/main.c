90f733c331bbd913a8c42967262b1cf86de394eb
cfcastaneda98 <cfcastaneda@miners.utep.edu>
2020-11-01 20:27:03 -0700

    Have made a header file to replace it into the notes on the State Machine file in order to composed the song. Fixed a typo in Main.

--- project/src/buzblinkToy/main.c
+++ project/src/buzblinkToy/main.c
@@ -3,7 +3,6 @@
 #include "led.h"
 #include "buzzer.h"
 #include "switches.h"
-#include "stateMachines.h"
 
 void main(void)
 {
@@ -12,6 +11,5 @@ void main(void)
   buzzer_init();
   switch_init();
   enableWDTInterrupts();
-  state_init();
   or_sr(0x18);   //CPU off, GIE on
 }
---
+++ project/src/buzblinkToy/notes.h
@@ -0,0 +1,17 @@
+#ifndef notes_included
+#define notes_included
+
+#define CNOTE 3817
+#define CSH 3610
+#define D 3405
+#define DSH 3205
+#define E 3033
+#define F 2863
+#define FSH 2778
+#define G 2551
+#define GSH 4807
+#define A 4545
+#define ASH 4290
+#define B 4049
+
+#endif
